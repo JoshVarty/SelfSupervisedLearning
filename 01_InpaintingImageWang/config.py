26497d0b6bed21958774084d0b562cbe56e975b2
Josh Varty <joshvarty@gmail.com>
2020-02-23 22:07:17 -0500

    Remove unused config params

---
+++ 01_InpaintingImageWang/config.py
@@ -0,0 +1,24 @@
+from fastai2.layers import Mish, MaxPool
+from fastai2.vision.models.xresnet import xresnet34
+
+config = {
+    'lr': 8e-3,
+    'size': 128,
+    'sqrmom': 0.99,
+    'mom': 0.9,
+    'eps': 1e-6,
+    'epochs': 15,
+    'bs': 64,
+    'mixup': 0.,
+    'opt': 'ranger',
+    'sh': 0.,
+    'sa': 0,
+    'sym': 0,
+    'beta': 0.,
+    'act_fn': Mish,
+    'fp16': 0,
+    'pool': MaxPool,
+    'dump': 0,
+    'runs': 1,
+    'model': xresnet34
+}
